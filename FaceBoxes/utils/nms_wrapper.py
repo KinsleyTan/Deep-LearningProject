67b2436369b4cf48f06055ee8aaca76ac20ef767
Altbryan22 <bryanaustinhidayat1@gmail.com>
2025-12-12 15:22:21 +0700

    update facemesh 3d

--- FaceBoxes/utils/nms_wrapper.py
+++ FaceBoxes/utils/nms_wrapper.py
@@ -1,19 +1,24 @@
-# coding: utf-8
-
-# --------------------------------------------------------
-# Fast R-CNN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick
-# --------------------------------------------------------
-
-from .nms.cpu_nms import cpu_nms, cpu_soft_nms
+import torch
+import torchvision
 
 
 def nms(dets, thresh):
-    """Dispatch to either CPU or GPU NMS implementations."""
-
+    """
+    Pure PyTorch NMS replacement for cpu_nms.
+    dets: numpy array [N, 5] -> [x1, y1, x2, y2, score]
+    thresh: float - IoU threshold
+    """
     if dets.shape[0] == 0:
         return []
-    return cpu_nms(dets, thresh)
-    # return gpu_nms(dets, thresh)
+
+    boxes = torch.tensor(dets[:, :4], dtype=torch.float32)
+    scores = torch.tensor(dets[:, 4], dtype=torch.float32)
+
+    keep = torchvision.ops.nms(boxes, scores, thresh)
+
+    return keep.numpy()
+
+
+# Soft-NMS fallback (not used, but required to avoid import errors)
+def cpu_soft_nms(dets, thresh, method=1, sigma=0.5):
+    return nms(dets, thresh)
--- demo_webcam_smooth.py
+++ demo_webcam_smooth.py
@@ -1,124 +1,106 @@
 # coding: utf-8
-
-__author__ = 'cleardusk'
-
 import argparse
-import imageio
 import cv2
 import numpy as np
-from tqdm import tqdm
-import yaml
 from collections import deque
+import yaml
 
 from FaceBoxes import FaceBoxes
 from TDDFA import TDDFA
 from utils.render import render
-# from utils.render_ctypes import render
 from utils.functions import cv_draw_landmark
 
 
 def main(args):
     cfg = yaml.load(open(args.config), Loader=yaml.SafeLoader)
 
-    # Init FaceBoxes and TDDFA, recommend using onnx flag
-    if args.onnx:
-        import os
-        os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-        os.environ['OMP_NUM_THREADS'] = '4'
+    # init
+    gpu_mode = args.mode == 'gpu'
+    tddfa = TDDFA(gpu_mode=gpu_mode, **cfg)
+    face_boxes = FaceBoxes()
 
-        from FaceBoxes.FaceBoxes_ONNX import FaceBoxes_ONNX
-        from TDDFA_ONNX import TDDFA_ONNX
+    cap = cv2.VideoCapture(0)
 
-        face_boxes = FaceBoxes_ONNX()
-        tddfa = TDDFA_ONNX(**cfg)
-    else:
-        gpu_mode = args.mode == 'gpu'
-        tddfa = TDDFA(gpu_mode=gpu_mode, **cfg)
-        face_boxes = FaceBoxes()
-
-    # Given a camera
-    # before run this line, make sure you have installed `imageio-ffmpeg`
-    reader = imageio.get_reader("<video0>")
-
-    # the simple implementation of average smoothing by looking ahead by n_next frames
-    # assert the frames of the video >= n
     n_pre, n_next = args.n_pre, args.n_next
     n = n_pre + n_next + 1
+
     queue_ver = deque()
     queue_frame = deque()
 
-    # run
     dense_flag = args.opt in ('2d_dense', '3d')
     pre_ver = None
-    for i, frame in tqdm(enumerate(reader)):
-        frame_bgr = frame[..., ::-1]  # RGB->BGR
+    first_frame = True
 
-        if i == 0:
-            # the first frame, detect face, here we only use the first face, you can change depending on your need
-            boxes = face_boxes(frame_bgr)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        img = frame.copy()
+
+        if first_frame:
+            boxes = face_boxes(img)
+            if len(boxes) == 0:
+                cv2.imshow("image", img)
+                if cv2.waitKey(1) == ord('q'):
+                    break
+                continue
+
             boxes = [boxes[0]]
-            param_lst, roi_box_lst = tddfa(frame_bgr, boxes)
-            ver = tddfa.recon_vers(param_lst, roi_box_lst, dense_flag=dense_flag)[0]
-
-            # refine
-            param_lst, roi_box_lst = tddfa(frame_bgr, [ver], crop_policy='landmark')
+            param_lst, roi_box_lst = tddfa(img, boxes)
             ver = tddfa.recon_vers(param_lst, roi_box_lst, dense_flag=dense_flag)[0]
 
             # padding queue
             for _ in range(n_pre):
                 queue_ver.append(ver.copy())
+                queue_frame.append(img.copy())
+
             queue_ver.append(ver.copy())
+            queue_frame.append(img.copy())
 
-            for _ in range(n_pre):
-                queue_frame.append(frame_bgr.copy())
-            queue_frame.append(frame_bgr.copy())
+            pre_ver = ver.copy()
+            first_frame = False
+
         else:
-            param_lst, roi_box_lst = tddfa(frame_bgr, [pre_ver], crop_policy='landmark')
-
-            roi_box = roi_box_lst[0]
-            # todo: add confidence threshold to judge the tracking is failed
-            if abs(roi_box[2] - roi_box[0]) * abs(roi_box[3] - roi_box[1]) < 2020:
-                boxes = face_boxes(frame_bgr)
-                boxes = [boxes[0]]
-                param_lst, roi_box_lst = tddfa(frame_bgr, boxes)
-
+            param_lst, roi_box_lst = tddfa(img, [pre_ver], crop_policy='landmark')
             ver = tddfa.recon_vers(param_lst, roi_box_lst, dense_flag=dense_flag)[0]
 
             queue_ver.append(ver.copy())
-            queue_frame.append(frame_bgr.copy())
+            queue_frame.append(img.copy())
 
-        pre_ver = ver  # for tracking
+            pre_ver = ver.copy()
 
-        # smoothing: enqueue and dequeue ops
+        # ---- smoothing & rendering ----
         if len(queue_ver) >= n:
             ver_ave = np.mean(queue_ver, axis=0)
+            base_frame = queue_frame[n_pre]  # FIX: always exists
 
             if args.opt == '2d_sparse':
-                img_draw = cv_draw_landmark(queue_frame[n_pre], ver_ave)  # since we use padding
+                out = cv_draw_landmark(base_frame, ver_ave)
             elif args.opt == '2d_dense':
-                img_draw = cv_draw_landmark(queue_frame[n_pre], ver_ave, size=1)
+                out = cv_draw_landmark(base_frame, ver_ave, size=1)
             elif args.opt == '3d':
-                img_draw = render(queue_frame[n_pre], [ver_ave], tddfa.tri, alpha=0.7)
-            else:
-                raise ValueError(f'Unknown opt {args.opt}')
+                out = render(base_frame, [ver_ave], tddfa.tri, alpha=0.7)
 
-            cv2.imshow('image', img_draw)
-            k = cv2.waitKey(20)
-            if (k & 0xff == ord('q')):
+            cv2.imshow("image", out)
+            if cv2.waitKey(1) == ord('q'):
                 break
 
             queue_ver.popleft()
             queue_frame.popleft()
 
+    cap.release()
+    cv2.destroyAllWindows()
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='The smooth demo of webcam of 3DDFA_V2')
-    parser.add_argument('-c', '--config', type=str, default='configs/mb1_120x120.yml')
-    parser.add_argument('-m', '--mode', default='cpu', type=str, help='gpu or cpu mode')
-    parser.add_argument('-o', '--opt', type=str, default='2d_sparse', choices=['2d_sparse', '2d_dense', '3d'])
-    parser.add_argument('-n_pre', default=1, type=int, help='the pre frames of smoothing')
-    parser.add_argument('-n_next', default=1, type=int, help='the next frames of smoothing')
-    parser.add_argument('--onnx', action='store_true', default=False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', default='configs/mb1_120x120.yml')
+    parser.add_argument('-m', '--mode', default='cpu')
+    parser.add_argument('-o', '--opt', default='3d',
+                        choices=['2d_sparse', '2d_dense', '3d'])
+    parser.add_argument('-n_pre', default=1, type=int)
+    parser.add_argument('-n_next', default=1, type=int)
 
     args = parser.parse_args()
     main(args)
